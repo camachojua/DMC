52b389daba2a1e8491c693434a0c5057b4073283
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-03-19 19:13:29 -0600

    Agrega mensaje de error pasa password corto.

--- app/src/main/java/com/example/dmc/MainActivity.kt
+++ app/src/main/java/com/example/dmc/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.dmc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_fragment)
+
+        next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = "Password demasiado corto."
+            } else {
+                password_text_input.error = null;
+            }
+        }
+
+        password_edit_text.addTextChangedListener {
+            if (isPasswordValid(password_edit_text.text!!))
+                password_text_input.error = null
+        }
+    }
+
+    private fun isPasswordValid(text: Editable?): Boolean{
+        return text != null && text.length >= 8;
     }
 }
